5da7bf02d3b29d787380d214a1b8baaec27233f3
juanosarg <31105173+juanosarg@users.noreply.github.com>
2022-08-23 18:50:46 +0200

    v1.0

--- 1.3/Source/VanillaAnimalsExpandedRoyal/VanillaAnimalsExpandedRoyal/AI/JobDrivers/JobDriver_Talk.cs
+++ 1.3/Source/VanillaAnimalsExpandedRoyal/VanillaAnimalsExpandedRoyal/AI/JobDrivers/JobDriver_Talk.cs
@@ -35,11 +35,18 @@ namespace VanillaAnimalsExpandedRoyal
                 recipient.relations.AddDirectRelation(InternalDefOf.VAERoy_TutorRelation, pawn);
                 SkillDef skillDef = DefDatabase<SkillDef>.AllDefs.Where((SkillDef x) => !recipient.skills.GetSkill(x).TotallyDisabled).RandomElement();
                 int level = recipient.skills.GetSkill(skillDef).Level;
-                recipient.skills.Learn(skillDef, 200f, direct: true);
-                MoteMaker.ThrowText(recipient.DrawPos, recipient.Map, skillDef.LabelCap + ": +200 xp", Color.white, 3.65f);
+                IntRange skillGain = new IntRange(160, 240);
+                int realSkillRange = skillGain.RandomInRange;
+                recipient.skills.Learn(skillDef, realSkillRange, direct: true);
+                MoteMaker.ThrowText(recipient.DrawPos, recipient.Map, skillDef.LabelCap + ": "+ realSkillRange + " xp", Color.white, 3.65f);
                 if (pawn.Name == null || pawn.Name.Numerical)
                 {
                     pawn.Name = PawnBioAndNameGenerator.GeneratePawnName(pawn);
+                }
+                if (!recipient.Dead && recipient.needs.joy != null)
+                {
+                    JoyKindDef joyKind = JoyKindDefOf.Social;
+                    recipient.needs.joy.GainJoy(0.05f, joyKind);
                 }
 
 
